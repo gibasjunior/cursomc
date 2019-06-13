0ba02b75879686ee6648edd1bea86e30d1e483f4
gibasjunior <gibasjunior@hotmail.com>
2019-06-13 10:10:49 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/gilberto/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.gilberto.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilberto.cursomc.domain.Categoria;
+import com.gilberto.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria buscar(Integer id) {
+	Categoria obj = repo.findOne(id);
+    return obj;
+	
+	
+	}
+	
+	
+}
